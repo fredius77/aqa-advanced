c9adf87572e2b095f14e2e9f4b1518b9448ceb2f
Frederik_Huralnyk <frederik_huralnyk@Frederik-Huralnyks-MacBook-Pro.local>
2025-06-09 18:43:09 +0300

    feat: додав обчислення площі, об'єму та приклади з конкатенацією

---
+++ hw4/task1.js
@@ -0,0 +1,11 @@
+let myString = "Це рядок";
+let myNumber = 42;
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+
+console.log("Рядок:", myString);
+console.log("Число:", myNumber);
+console.log("Буль:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
---
+++ hw4/task3.js
@@ -0,0 +1,5 @@
+let age = 18;
+let isAdult = age >= 18;
+
+console.log("Вік особи:", age);
+console.log("Чи повнолітня особа:", isAdult);
