c8e42b697db4d10a98c52bf90a4c918c2fa2745b
Frederik_Huralnyk <frederik_huralnyk@Frederik-Huralnyks-MacBook-Pro.local>
2025-06-12 19:43:48 +0300

    changes: ДЗ 5.1. JavaScript conditions and loops

--- hw5/task1.js
+++ hw5/task1.js
@@ -1,5 +1,5 @@
 
-let averageGrade = 870;
+let averageGrade = 61;
 
 // v1
 if (averageGrade < 60) {
@@ -21,16 +21,16 @@ switch (true) {
     case (averageGrade < 60):
       console.log("Незадовільно");
       break;
-    case (averageGrade >= 60 && averageGrade <= 70):
+    case (averageGrade <= 70):
       console.log("Задовільно");
       break;
-    case (averageGrade >= 71 && averageGrade <= 80):
+    case (averageGrade <= 80):
       console.log("Добре");
       break;
-    case (averageGrade >= 81 && averageGrade <= 90):
+    case (averageGrade <= 90):
       console.log("Дуже добре");
       break;
-    case (averageGrade >= 91 && averageGrade <= 100):
+    case (averageGrade <= 100):
       console.log("Відмінно");
       break;
     default:
