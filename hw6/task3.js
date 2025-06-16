76b528a982a083d6f41a5d15334a97539c57db7f
Frederik_Huralnyk <frederik_huralnyk@Frederik-Huralnyks-MacBook-Pro.local>
2025-06-16 19:03:41 +0300

    feat: early return pattern

--- hw6/task3.js
+++ hw6/task3.js
@@ -1,12 +1,11 @@
 function checkOrder(available, ordered) {
-    if (ordered === 0) {
-        return "Your order is empty";
-    } else if (ordered > available) {
-        return "Your order is too large, we don’t have enough goods.";
-    } else {
-        return "Your order is accepted";
-    }
+    if (ordered === 0) return "Your order is empty";
+
+    if (ordered > available) return "Your order is too large";
+
+    return "Your order is accepted";
 }
+
 
 
 console.log(checkOrder(100, 0));     
