c4185c5cc53751bc6ac48dcd7a5bb3a98b2e6270
Frederik_Huralnyk <frederik_huralnyk@Frederik-Huralnyks-MacBook-Pro.local>
2025-06-30 17:46:09 +0300

    feat: hw 9

---
+++ hw9/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Майстер і Маргарита",
+    author: "Михайло Булгаков",
+    year: "1937"
+};
+
+const { title, author } = book;
+
+console.log("Назва книги:", title);
+console.log("Автор книги:", author);
